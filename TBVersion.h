0e9ae2cb66aff01e665909c0be1a0ace7a4c8106
petrSchreiber <petr.schreiber@gmail.com>
2017-07-22 12:24:54 +0200

    feat: added version files

---
+++ TBVersion.h
@@ -0,0 +1,8 @@
+// This file is create automatically by the build process. Do not edit!
+
+#define  THIN_VERSIONMAJOR                1
+#define  THIN_VERSIONMINOR                10
+#define  THIN_VERSIONBUILD                1
+#define  THIN_VERSIONREVISION             0
+#define  THIN_VERSIONLITERAL              "1.10.1.0\0"
+#define  THIN_VERSIONLITERAL4VL           "1.10\0"
