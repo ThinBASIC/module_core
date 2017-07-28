6d5f75efef0af90d4360b7a693612a87a1715e42
Eros Olmi <eros.olmi@thinbasic.com>
2017-07-28 21:26:04 +0200

    new version

--- TBVersion.h
+++ TBVersion.h
@@ -2,7 +2,7 @@
 
 #define  THIN_VERSIONMAJOR                1
 #define  THIN_VERSIONMINOR                10
-#define  THIN_VERSIONBUILD                1
+#define  THIN_VERSIONBUILD                2
 #define  THIN_VERSIONREVISION             0
-#define  THIN_VERSIONLITERAL              "1.10.1.0\0"
+#define  THIN_VERSIONLITERAL              "1.10.2.0\0"
 #define  THIN_VERSIONLITERAL4VL           "1.10\0"
